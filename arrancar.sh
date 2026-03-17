1912c04dc106817209b244b61501657fb754bd31
Dani Tabuyo <dtabuyodesigner@gmail.com>
2026-03-17 18:31:58 +0000

    fix: exportador CSV sin columna ID + arrancar_windows.bat modo developer

---
+++ arrancar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Cuaderno del Tutor - Script de arranque para Linux
+# Coloca este archivo en la raíz del proyecto: ~/Documentos/APP_EVALUAR/
+
+PROJECT_DIR="$HOME/Documentos/APP_EVALUAR"
+VENV="$PROJECT_DIR/venv/bin/python"
+
+cd "$PROJECT_DIR"
+"$VENV" desktop.py
